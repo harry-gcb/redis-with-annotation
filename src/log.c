4062d3d5be9651fec2362ce27ca8b0ccfb1a2660
gaochunbo <1814593561@qq.com>
2022-04-04 14:58:01 +0800

    modify debug log

--- src/log.c
+++ src/log.c
@@ -1,7 +1,6 @@
 #include "log.h"
 
 #ifdef ENABLE_DEBUG
-
 #include <stdio.h>
 #include <stdint.h>
 #include <sys/types.h>
@@ -16,7 +15,7 @@
 
 #include <sys/uio.h>
 
-static const char* logstr[] = {
+static const char *logstr[] = {
     "FATAL",
     "ERROR",
     "WARN",
@@ -52,8 +51,8 @@ Bright Cyan     96  106
 Bright White    97  107
 */
 
-#define LOG_DATA_LEN      (4*1024)
-#define LOG_HEAD_LEN      128
+#define LOG_DATA_LEN (4 * 1024)
+#define LOG_HEAD_LEN 128
 static char NEWLINE[1] = {'\n'};
 static const char *nocolor = "\033[0m";
 static const char *logcolor[] = {
@@ -66,81 +65,86 @@ static const char *logcolor[] = {
 };
 
 #ifndef CONSOLE_DEBUG
-__attribute__((constructor))
-static void debug_constructor(void) {
+__attribute__((constructor)) static void debug_constructor(void)
+{
     fd = open(filename, O_RDWR | O_CREAT | O_APPEND, 0644);
-    if (fd < 0) {
+    if (fd < 0)
+    {
         return;
     }
     console = 0;
 }
 
-__attribute__((destructor)) 
-static void debug_destructor(void) {
-    if (!(fd < 0)) {
+__attribute__((destructor)) static void debug_destructor(void)
+{
+    if (!(fd < 0))
+    {
         close(fd);
     }
 }
 #endif
 
-void setLogLevel(unsigned int level) {
+void setLogLevel(unsigned int level)
+{
     logLevel = level;
 }
 
-void logMessage(unsigned int level, const char *filename, int line, const char *function, pthread_t tid, const char *fmt, ...) {
-    if (level > logLevel) {
+void logMessage(unsigned int level, const char *filename, int line, const char *function, pthread_t tid, const char *fmt, ...)
+{
+    if (level > logLevel)
+    {
         return;
     }
-    struct timeval tv = { 0 };
+    struct timeval tv = {0};
     gettimeofday(&tv, NULL);
     struct tm *tm = localtime((const time_t *)&tv.tv_sec);
-    if (!tm) {
+    if (!tm)
+    {
         return;
     }
 
-    char data[LOG_DATA_LEN] = { 0 };
-    char head[LOG_HEAD_LEN] = { 0 };
+    char data[LOG_DATA_LEN] = {0};
+    char head[LOG_HEAD_LEN] = {0};
     va_list args;
     va_start(args, fmt);
     int data_size = vsnprintf(data, sizeof(data), fmt, args);
     va_end(args);
 
-    int head_size = console ? snprintf(head, sizeof(head), "%s[%04d-%02d-%02d %02d:%02d:%02d.%06ld]%s %-5s %s (%s:%d) [%ld] ", 
-                                                  logcolor[level],
-                                                  tm->tm_year+1900, 
-                                                  tm->tm_mon+1, 
-                                                  tm->tm_mday,
-                                                  tm->tm_hour, 
-                                                  tm->tm_min, 
-                                                  tm->tm_sec, 
-                                                  tv.tv_usec,
-                                                  nocolor,
-                                                  logstr[level], 
-                                                  function, 
-                                                  filename, 
-                                                  line, 
-                                                  tid)
-                    : snprintf(head, sizeof(head), "[%04d-%02d-%02d %02d:%02d:%02d.%06ld] %-5s %s (%s:%d) [%ld] ", 
-                                                  tm->tm_year+1900, 
-                                                  tm->tm_mon+1, 
-                                                  tm->tm_mday,
-                                                  tm->tm_hour, 
-                                                  tm->tm_min, 
-                                                  tm->tm_sec, 
-                                                  tv.tv_usec,
-                                                  logstr[level], 
-                                                  function, 
-                                                  filename, 
-                                                  line, 
-                                                  tid);
-    struct iovec vec[3] = { 0 };
+    int head_size = console ? snprintf(head, sizeof(head), "%s[%04d-%02d-%02d %02d:%02d:%02d.%06ld]%s %-5s %s (%s:%d) [%ld] ",
+                                       logcolor[level],
+                                       tm->tm_year + 1900,
+                                       tm->tm_mon + 1,
+                                       tm->tm_mday,
+                                       tm->tm_hour,
+                                       tm->tm_min,
+                                       tm->tm_sec,
+                                       tv.tv_usec,
+                                       nocolor,
+                                       logstr[level],
+                                       function,
+                                       filename,
+                                       line,
+                                       tid)
+                            : snprintf(head, sizeof(head), "[%04d-%02d-%02d %02d:%02d:%02d.%06ld] %-5s %s (%s:%d) [%ld] ",
+                                       tm->tm_year + 1900,
+                                       tm->tm_mon + 1,
+                                       tm->tm_mday,
+                                       tm->tm_hour,
+                                       tm->tm_min,
+                                       tm->tm_sec,
+                                       tv.tv_usec,
+                                       logstr[level],
+                                       function,
+                                       filename,
+                                       line,
+                                       tid);
+    struct iovec vec[3] = {0};
     vec[0].iov_base = head;
     vec[0].iov_len = head_size;
     vec[1].iov_base = data;
     vec[1].iov_len = data_size;
     vec[2].iov_base = NEWLINE;
     vec[2].iov_len = sizeof(NEWLINE);
-    writev(fd, vec, 3);                          
+    writev(fd, vec, 3);
 }
-
 #endif
--- src/log.h
+++ src/log.h
@@ -3,11 +3,12 @@
 
 #include <pthread.h>
 
-typedef enum LogLevel {
+typedef enum LogLevel
+{
     LOG_LEVEL_FATAL = 0,
     LOG_LEVEL_ERROR = 1,
-    LOG_LEVEL_WARN  = 2,
-    LOG_LEVEL_INFO  = 3,
+    LOG_LEVEL_WARN = 2,
+    LOG_LEVEL_INFO = 3,
     LOG_LEVEL_DEBUG = 4,
     LOG_LEVEL_TRACE = 5,
 } LOG_LEVEL;
@@ -15,11 +16,10 @@ typedef enum LogLevel {
 #ifdef ENABLE_DEBUG
 
 extern void logMessage(unsigned int level, const char *filename, int line, const char *function, pthread_t tid, const char *fmt, ...);
-#define LOG_FILE                   (strrchr("/" __FILE__,'/')+1)
-#define LOG_LEVEL(level)           LOG_LEVEL_##level, LOG_FILE, __LINE__, __func__, pthread_self()
-#define LOG(level, ...)      logMessage(LOG_LEVEL(level), __VA_ARGS__)
-#else 
-#define LOG(level, ...)         
+#define LOG_FILE (strrchr("/" __FILE__, '/') + 1)
+#define LOG_LEVEL(level) LOG_LEVEL_##level, LOG_FILE, __LINE__, __func__, pthread_self()
+#define LOG(level, ...) logMessage(LOG_LEVEL(level), __VA_ARGS__)
+#else
+#define LOG(level, ...)
 #endif
-
 #endif
